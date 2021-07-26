5d6a4cb9c86f01789aeece9ee24929396a84fa87
Martin Bond <28291143+martinbond7@users.noreply.github.com>
2021-07-26 10:19:25 +0100

    Suppress unused toolchain message, improved build script

--- build.sh
+++ build.sh
@@ -19,24 +19,32 @@
 
 set -o errexit
 set -o nounset
-USAGE="Usage: (basename $0) [ clean | debug | release ]"
+USAGE="Usage: (basename $0) [-v | --verbose] [ reset | clean | debug | release ]"
 
 CMAKE=cmake
 BUILD=./build
 TYPE=DEBUG
 BUILD_DIR=$BUILD/debug
 CLEAN=
+RESET=
+VERBOSE=
 
 for arg; do
   case "$arg" in
-    --help|-h)  echo $USAGE; exit 0;;
-    debug)      TYPE=DEBUG;   BUILD_DIR=$BUILD/debug ;;
-    release)    TYPE=RELEASE; BUILD_DIR=$BUILD/release ;;
-    clean)      CLEAN=1  ;;
-    *)          echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
+    --help|-h)    echo $USAGE; exit 0;;
+    -v|--verbose) VERBOSE='VERBOSE=1'  ;;
+    debug)        TYPE=DEBUG;   BUILD_DIR=$BUILD/debug ;;
+    release)      TYPE=RELEASE; BUILD_DIR=$BUILD/release ;;
+    clean)        CLEAN=1  ;;
+    reset)        RESET=1 ;;
+    *)            echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
   esac
 done
 
+[[ -d $BUILD_DIR ]] && rm -rf $BUILD_DIR
+    
 $CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_TOOLCHAIN_FILE=toolchain-STM32F407.cmake
+
 [[ -n $CLEAN ]] && $CMAKE --build $BUILD_DIR --target clean
-$CMAKE --build $BUILD_DIR -- VERBOSE=1
+
+$CMAKE --build $BUILD_DIR -- $VERBOSE
